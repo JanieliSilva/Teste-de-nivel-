a89d12e353da9f1a48f279b3ab4cddc109c72984
Tata <112900054+JanieliSilva@users.noreply.github.com>
2023-01-26 15:07:10 -0300

    Add files via upload

---
+++ multiplicação.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+
+int multiplicacao(int a, int b){
+	return a * b;
+}
+int calcula(int x, int y, int (*operacao)(int, int)){
+	return (*operacao)(x, y);
+}
+
+int main(){
+	int resultado = calcula(5, 3, multiplicacao);
+	printf("Resultado = %d", resultado);
+	return 0;
+}
